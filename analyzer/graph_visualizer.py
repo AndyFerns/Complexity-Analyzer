9de2e66b62e8ca14dc907c5a850178cfeb7e8e38
AndyFerns <write2andrew.fernandes@gmail.com>
2025-07-12 19:25:58 +0530

    graph display module

---
+++ analyzer/graph_visualizer.py
@@ -0,0 +1,26 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+def draw_call_graph(call_graph, complexities):
+    G = nx.DiGraph()
+    
+    # Add nodes with complexity graphs
+    for func, calls in call_graph.items():
+        label = f"{func} [ {complexities.get(func, 'O(?)')} ]"
+        
+        G.add_node(func, label=label)
+        for i in calls:
+            G.add_edge(func, i)
+            
+    pos = nx.spring_layout(G, k=1.5)
+    
+    # Nodes + labels
+    labels = {node: f"{node}\n{complexities.get(node, 'O(?)')}" for node in G.nodes()}
+    nx.draw_networkx_nodes(G, pos, node_color='skyblue', node_size=2000)
+    nx.draw_networkx_edges(G, pos, arrowstyle="->", arrowsize=20)
+    nx.draw_networkx_labels(G, pos, labels, font_size=10, font_weight='bold')
+
+    plt.title("Function Call Graph with Complexities")
+    plt.axis('off')
+    plt.tight_layout()
+    plt.show()
