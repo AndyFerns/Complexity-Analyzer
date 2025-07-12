cc17a79168d2e9960dc23b71b2cfa1aa4c2d62fc
AndyFerns <write2andrew.fernandes@gmail.com>
2025-07-12 19:26:09 +0530

    edge case for testing

---
+++ test/graph_test.py
@@ -0,0 +1,11 @@
+def main():
+    arr = [5, 3, 8]
+    merge_sort(arr)
+
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        L = arr[:mid]
+        R = arr[mid:]
+        merge_sort(L)
+        merge_sort(R)
