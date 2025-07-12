96247e3fd98829be29c902681f293e3463682e36
AndyFerns <write2andrew.fernandes@gmail.com>
2025-07-12 18:32:45 +0530

    sample code to test T(n) = 2T(n/2) + O(n)

---
+++ test/merge_sort.py
@@ -0,0 +1,8 @@
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        L = arr[:mid]
+        R = arr[mid:]
+        merge_sort(L)
+        merge_sort(R)
+        # merge step
