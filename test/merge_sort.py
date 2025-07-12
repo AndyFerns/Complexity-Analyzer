f330f5588cdc751a1e2e42350f2708ba9592f3bf
AndyFerns <write2andrew.fernandes@gmail.com>
2025-07-12 18:58:46 +0530

    merge sort code for testing

--- test/merge_sort.py
+++ test/merge_sort.py
@@ -5,4 +5,12 @@ def merge_sort(arr):
         R = arr[mid:]
         merge_sort(L)
         merge_sort(R)
-        # merge step
+        i = j = k = 0
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
