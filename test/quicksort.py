14873d746c1971100c08a2d5adbe3dadb3e1254f
AndyFerns <write2andrew.fernandes@gmail.com>
2025-07-13 22:25:42 +0530

    quicksort algo for testing

---
+++ test/quicksort.py
@@ -0,0 +1,10 @@
+def main():
+    quicksort([1, 5, 3, 2, 9])
+
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[0]
+    left = [x for x in arr[1:] if x < pivot]
+    right = [x for x in arr[1:] if x >= pivot]
+    return quicksort(left) + [pivot] + quicksort(right)
