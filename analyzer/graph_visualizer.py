b6eafbc1ddc9a305fcad8dbadf10e7b8d31ea1df
AndyFerns <write2andrew.fernandes@gmail.com>
2025-07-13 22:23:51 +0530

    added colour coded nodes

--- analyzer/graph_visualizer.py
+++ analyzer/graph_visualizer.py
@@ -20,22 +20,23 @@ def classify_complexity(complexity_str):
     
 def draw_call_graph(call_graph, complexities):
     G = nx.DiGraph()
-    
-    # Add nodes with complexity graphs
+    node_colors = []
+
+    # Add nodes and edges
     for func, calls in call_graph.items():
-        label = f"{func} [ {complexities.get(func, 'O(?)')} ]"
-        
-        G.add_node(func, label=label)
-        for i in calls:
-            G.add_edge(func, i)
-            
+        complexity = complexities.get(func, "O(?)")
+        G.add_node(func, label=f"{func}\n{complexity}")
+        node_colors.append(classify_complexity(complexity))
+        for callee in calls:
+            G.add_edge(func, callee)
+
     pos = nx.spring_layout(G, k=1.5)
-    
-    # Nodes + labels
+
+    # Draw graph with color-coded nodes
     labels = {node: f"{node}\n{complexities.get(node, 'O(?)')}" for node in G.nodes()}
-    nx.draw_networkx_nodes(G, pos, node_color='skyblue', node_size=2000)
+    nx.draw_networkx_nodes(G, pos, node_color=node_colors, node_size=2000) # type: ignore 
     nx.draw_networkx_edges(G, pos, arrowstyle="->", arrowsize=20)
-    nx.draw_networkx_labels(G, pos, labels, font_size=10, font_weight='bold')
+    nx.draw_networkx_labels(G, pos, labels, font_size=9, font_weight='bold')
 
     plt.title("Function Call Graph with Complexities")
     plt.axis('off')
